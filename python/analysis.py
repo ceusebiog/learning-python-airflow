1998f0759ef99ffb5958ac3644bb27e808745f4e
Claudio Eusebio <claudio_eusebio@outlook.com>
2024-10-18 23:34:47 -0500

    feat: cross validation

--- python/analysis.py
+++ python/analysis.py
@@ -1,12 +1,17 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import numpy as np
 import os
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+from sqlalchemy import create_engine
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+
 from imblearn.over_sampling import SMOTE
+
 
 DB_USER = os.getenv('POSTGRES_USER', 'user')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'password')
@@ -54,12 +59,12 @@ def train_model(df):
     
   return model
 
+"""Función para afinar el modelo con GridSearchCV."""
 def tune_model(X_train, y_train):
-  """Función para afinar el modelo con GridSearchCV."""
   param_grid = {
-      'n_estimators': [50, 100, 150],
-      'max_depth': [None, 10, 20],
-      'min_samples_split': [2, 5, 10]
+    'n_estimators': [50, 100, 150],
+    'max_depth': [None, 10, 20],
+    'min_samples_split': [2, 5, 10]
   }
   rf = RandomForestClassifier(random_state=42)
   grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5)
@@ -67,6 +72,29 @@ def tune_model(X_train, y_train):
   print("Mejores parámetros:", grid_search.best_params_)
   return grid_search.best_estimator_
 
+"""Función para validar el modelo usando validación cruzada."""
+def validate_model(df):
+  X = df[['amount', 'transaction_type_encoded', 'is_night']]
+  y = df['is_fraud']
+  
+  # Aplicar SMOTE para balancear las clases
+  smote = SMOTE(random_state=42)
+  X_balanced, y_balanced = smote.fit_resample(X, y)
+
+  # Crear el modelo usando los mejores hiperparámetros encontrados
+  model = RandomForestClassifier(n_estimators=50, max_depth=None, min_samples_split=2, random_state=42)
+
+  # Validación cruzada con 5 particiones
+  cv_scores = cross_val_score(model, X_balanced, y_balanced, cv=5, scoring='accuracy')
+
+  print("Puntuaciones de validación cruzada:", cv_scores)
+  print("Precisión promedio:", np.mean(cv_scores))
+  
+  return cv_scores
+
 if __name__ == '__main__':
     df = prepare_dataset()
+
+    validate_model(df)
+
     model = train_model(df)
